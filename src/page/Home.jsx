12fd3a5b46562cc5f31e54355423e0a994fe658f
Ahmed koraish <136814559+Ahmedkoraish@users.noreply.github.com>
2025-11-20 14:48:48 +0200

    feat: add text, portfolio, and about sections to home page

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,9 +1,14 @@
-
 import React from 'react'
 import Hero from '../component/Hero'
+import Text from '../component/Text'
+import About from '../component/About '
+import Portfolio from '../component/Portfolio'
 
 export default function Home() {
     return <>
         <Hero />
+        <Text />
+        <About/>
+        <Portfolio/>
     </>
 }
